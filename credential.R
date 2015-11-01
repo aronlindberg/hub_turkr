9e5a2ec6228ae57b550a6d3b06fb838bac9c054d
bilakhiaricky <rbilakhi@stevens.edu>
2015-11-01 16:55:34 -0500

    interactive code, will ask fro AWS Credentials

--- credential.R
+++ credential.R
@@ -1,5 +1,8 @@
 #credential use for the requester consist of AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
+<<<<<<< HEAD
 #interactive code
+=======
+>>>>>>> fa807a15928ebdd53a3feb9f8b4f80196a0ea7eb
 #https://aws.amazon.com/
 AWS_ACCESS_KEY_ID <- readline("Enter your AWS_ACCESS_KEY_ID:")
 AWS_SECRET_ACCESS_KEY <- readline("Enter your AWS_SECRET_ACCESS_KEY:")
