530e2a8f90739c606cec9b9d4488791f8a17f21b
bilakhiaricky <rbilakhi@stevens.edu>
2015-11-01 17:30:43 -0500

    main file consist of all the R file to exceute

--- execute.R
+++ execute.R
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
+
 #main file consisiting of all the file to execute
 #remove all the variable from the environment.
 rm(list =ls()) 
-=======
-
-#remove all the variable from the environment.
-#rm(list =ls()) 
->>>>>>> fa807a15928ebdd53a3feb9f8b4f80196a0ea7eb
 
 script_list <- list("initialization.R","credential.R","qualification.R",
                     "Bulk_HIT_Creation.R","HIT_Status.R","Get_Assignment.R")
