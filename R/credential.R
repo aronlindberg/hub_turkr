a1b9092f6e46b95c6c324c0b0b00afc16cbf5725
bilakhiaricky <rbilakhi@stevens.edu>
2015-11-11 01:09:18 -0500

    credential file consisting of key value pair.
    credential file consisting of key value pair.
    load the rootkey file in folder R.
    Issue # Create a way to store credentials safely #5

---
+++ R/credential.R
@@ -0,0 +1,22 @@
+#credential use for the requester consist of AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
+#https://aws.amazon.com/
+#checking whether the key pair file exists or not. if yes, then login to the AWS Mechanical Turk.
+
+#checking for the rootkey file having credentials in it.
+#do not change anything in the file or format of the file
+
+if (file.exists("rootkey.csv")) {
+  key_pair <- read.csv("rootkey.csv", header = F, sep = "=")
+} else if (file.exists("rootkey.csv.txt")){
+  key_pair <- read.delim( "rootkey.csv.txt", header = F, sep="=" )
+} else{
+  print("File consisting of credentials does not exist")
+}
+
+#initializing credentials
+credentials( keypair=c(key_pair$V2[1], key_pair$V2[2]))
+
+#checking the balance for the account
+while (AccountBalance() == 0){
+  print("Check out your key file or Recharge your Account")
+}
