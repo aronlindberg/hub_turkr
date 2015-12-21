9cbbb0c849872b47f20f175586300c728eec217f
bilakhiaricky <rbilakhi@stevens.edu>
2015-12-21 17:01:45 -0500

    Execute Main file consist of files for the flow of the program
    Execute Main file consist of files for the flow of the program

--- R/execute.R
+++ R/execute.R
@@ -14,8 +14,7 @@
 rm(list =ls()) 
 
 script_list <- list("working_directory.R", "initialization.R", "credential.R", 
-                    "qualification.R", "Bulk_HIT_Creation.R", "HIT_Status.R",
-                     "Get_Assignment.R")
+                    "Pre_Qualification.R", "Bulk_HIT_Creation.R")
 
 for (loop_variable in 1:length(script_list)){
   try(source(script_list[[loop_variable]]))
