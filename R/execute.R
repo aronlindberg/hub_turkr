d1392d0bd3a762f626760738a0ccc01336aa0a25
bilakhiaricky <rbilakhi@stevens.edu>
2016-01-20 12:18:29 -0500

    setting up sequence of files to run
    setting up sequence of files to run

--- R/execute.R
+++ R/execute.R
@@ -10,11 +10,11 @@
 # credentials from AWS in `credential.R`
 #keep the rootkey file extracted from Amazon Web service in R folder. 
 
-
+getwd()
 rm(list =ls()) 
 
 script_list <- list("working_directory.R", "initialization.R", "credential.R", 
-                    "Pre_Qualification.R", "Bulk_HIT_Creation.R")
+                     "Pre_Qualification.R","Bulk_HIT_Creation.R")
 
 for (loop_variable in 1:length(script_list)){
   try(source(script_list[[loop_variable]]))
