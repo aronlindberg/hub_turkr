c3371a447c970093a85ab495a49a21bebaaa4ce4
bilakhiaricky <rbilakhi@stevens.edu>
2016-01-21 12:01:56 -0500

    Environment Setup R code
    Code written to set up the environment for the hub_miner

--- Hub_Miner/execute.R
+++ Hub_Miner/execute.R
@@ -1,5 +1,8 @@
+
+#set up your working directory
 setwd("C:/Users/Ricky/Desktop/Hub_Miner/")
 
+# required package for hub_miner
 required_packages <- c('github','roxygen2', 'plyr','httpuv','httr')
 
 new_packages <- required_packages[!(required_packages %in% installed.packages()[,'Package'])]
@@ -13,6 +16,7 @@ if(required_packages == c('github'))
   install_github("cscheid/rgithub")
 }
 
+# required library
 library(github)
 library(httpuv)
 library(httr)
